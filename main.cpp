074f5ce11677850be1d471214ed3c3da53307e6d
AbdElAziz Moftah <vb.promax@gmail.com>
2018-01-09 04:06:57 +0200

    pre file saver

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 #include "taco_class.cpp"
 using namespace std;
 
 int main()
 {
-    int N = 9;
+    int N = 9, patchSize = 100000;
     short *board = new short[N * N];
+    short *buffer = new short[N * N * patchSize];
 
     Tico myTicTacToe(N, 5);
 
-    long c = 1000000L;
 
     int *range = new int[N*N];
     for(int i =0; i<N*N; i++){
         range[i] = i;
     }
 
+    long c = 100000000L;
+    long index = 0;
     while(c--){
         myTicTacToe.clearAllMatrix();
         random_shuffle(range, range + N*N);
@@ -24,6 +27,17 @@ int main()
         for(int i =0,j = 1; i<N*N; i++, j*=-1){
             int x = myTicTacToe.updateBoard(board, range[i], j);
             if(x == 1) break;
+        }
+
+        short *temp = myTicTacToe.getWinSeq();
+        for(int i =0; i < N * N; i++)
+            buffer[index++] = temp[i];
+
+        if(index >= patchSize){
+            //********************
+            index = 0;
+            /// Write to a file.
+            //********************
         }
 
     }
